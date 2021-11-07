ddbaa40676eacb83fcf6960110913d29e2eb9017
yeon <jryoun0404@gmail.com>
2021-11-08 01:06:50 +0900

    add testSaveDayModel method

--- be-you/be-youTests/TestCoreDataStack.swift
+++ be-you/be-youTests/TestCoreDataStack.swift
@@ -21,7 +21,37 @@ final class TestCoreDataStack: XCTestCase {
         bag = []
         coreDataManager = CoreDataManager(name: "Model", in: .inMemory)
     }
-
+    
+    func testSaveDayModel() {
+        expectation(forNotification: .NSManagedObjectContextDidSave,
+                    object: coreDataManager.viewContext) { _ in
+            return true
+        }
+        
+        let action: Action = {
+            let day: Day = self.coreDataManager.createEntity()
+            day.hashtags = ["빡침", "짜증나", "퇴사"]
+            day.date = Date()
+            day.diary = "정말 화가 많이 난다,, 퇴사하고 싶다"
+            day.rgbHEX = "AB4253"
+        }
+        
+        coreDataManager
+            .publisher(save: action)
+            .sink { completion in
+                if case .failure(let error) = completion {
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { success in
+                XCTAssertTrue(success)
+            }
+            .store(in: &bag)
+        
+        waitForExpectations(timeout: 2.0) { error in
+            XCTAssertNil(error, "저장 안됨")
+        }
+    }
+    
     override func tearDown() {
         super.tearDown()
         coreDataManager = nil
