c6dab3aa23f1efe5c74b453554f5582ac7e63def
yeon <jryoun0404@gmail.com>
2021-11-08 01:07:16 +0900

    add testFetchDayModel method

--- be-you/be-youTests/TestCoreDataStack.swift
+++ be-you/be-youTests/TestCoreDataStack.swift
@@ -52,6 +52,46 @@ final class TestCoreDataStack: XCTestCase {
         }
     }
     
+    func testFetchDayModel() {
+        // save
+        let action: Action = {
+            let day: Day = self.coreDataManager.createEntity()
+            day.hashtags = ["빡침", "짜증나", "퇴사"]
+            day.date = Date()
+            day.diary = "정말 화가 많이 난다,, 퇴사하고 싶다"
+            day.rgbHEX = "AB4253"
+        }
+        
+        coreDataManager
+            .publisher(save: action)
+            .sink { completion in
+                if case .failure(let error) = completion {
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { success in
+                XCTAssertTrue(success)
+            }
+            .store(in: &bag)
+        
+        // fetch
+        let request = NSFetchRequest<Day>(entityName: Day.entityName)
+        coreDataManager
+            .publisher(fetch: request)
+            .sink { completion in
+                if case .failure(let error) = completion {
+                    XCTAssertNil(error)
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { [weak self] days in
+                self?.dayList = days
+            }
+            .store(in: &bag)
+        
+        XCTAssertTrue(self.dayList.count == 1)
+        XCTAssertEqual(self.dayList.first?.rgbHEX, "AB4253")
+        XCTAssertEqual(self.dayList.first?.diary, "정말 화가 많이 난다,, 퇴사하고 싶다")
+    }
+    
     override func tearDown() {
         super.tearDown()
         coreDataManager = nil
