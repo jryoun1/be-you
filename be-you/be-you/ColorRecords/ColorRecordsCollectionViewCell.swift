fb37f3146ac34d9b6f506ac5941bca1b07f89ca2
yeon <jryoun0404@gmail.com>
2021-12-01 23:13:48 +0900

    add properties

--- be-you/be-you/ColorRecords/ColorRecordsCollectionViewCell.swift
+++ be-you/be-you/ColorRecords/ColorRecordsCollectionViewCell.swift
@@ -8,6 +8,21 @@
 import UIKit
 
 final class ColorRecordsCollectionViewCell: UICollectionViewCell {
+    //MARK: - 크기를 고정사이즈로 할지 고민좀 해봐야할듯..
+    private enum Design {
+        enum Size {
+            static let circleWidth: CGFloat = 84
+        }
+    }
+    static let identifier = "ColorRecordsCollectionViewCell"
+    
+    private var colorView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = Design.Size.circleWidth/2
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
