47588fe38861ba9100812fb0cebc27e423771165
yeon <jryoun0404@gmail.com>
2021-12-01 23:12:25 +0900

    create ColorRecordsCollecitonView cell file

---
+++ be-you/be-you/ColorRecords/ColorRecordsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ColorRecordsCollectionViewCell.swift
+//  be-you
+//
+//  Created by Yeon on 2021/11/30.
+//
+
+import UIKit
+
+class ColorRecordsCollectionViewCell: UICollectionViewCell {
+
+}
