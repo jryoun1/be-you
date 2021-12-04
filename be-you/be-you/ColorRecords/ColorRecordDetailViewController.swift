11906bd88b764a812f464075b6b55b660886eb97
yeon <jryoun0404@gmail.com>
2021-12-04 19:40:43 +0900

    create ColorRecordDetailViewController file

---
+++ be-you/be-you/ColorRecords/ColorRecordDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ColorRecordDetailViewController.swift
+//  be-you
+//
+//  Created by Yeon on 2021/12/04.
+//
+
+import UIKit
+
+final class ColorRecordDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
